67e52c8097e8f456da0316159f5f37a1d4e523ad
Luca <lucabk04@gmail.com>
2022-10-20 19:45:34 +0200

    EserciziWhile Final

--- src/EserciziWhile.java
+++ src/EserciziWhile.java
@@ -5,18 +5,51 @@ public class EserciziWhile {
         Scanner input = new Scanner(System.in);
         System.out.println("Quante volte vuoi far girare il programma?");
         int n = input.nextInt();
-        int i = 0;
 
-        // print the values from 0 to n and then from n to 0
-        while (i <= n) {
-            System.out.println(i);
-            i = i + 1;
-        }
-        while (i >= 0) {
-            i = i - 1;
-            System.out.println(i);
 
+        System.out.println("Stampiamo a video gli interi da 0 a n");
+        // Stampiamo a video gli interi da 0 a n
+        for (int i=0; i<=n; i++) {
+            System.out.println(i);
         }
 
+        System.out.println("Stampiamo a video gli interi da n a 0");
+        // Stampiamo a video gli interi da n a 0
+        System.out.println();
+        int j=n;
+        while (j >= 0) {
+            System.out.println(j);
+            j--;
+        }
+
+        System.out.println("Stampiamo a video i numeri interi dispari da 0 a n");
+
+        for (int i = 0; i <= n; i++){
+            if (i%2 != 0){
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Stampiamo il fattoriale di n");
+        int i = 1;
+        int F = n;
+        while (i < n){
+            F = F * i;
+            i++;
+        }
+
+        // for(int i=1; i < n; i++) {
+        // F = F * i;
+        // }
+        System.out.println("Il fattoriale di " + n + " e' " + F);
+        //Fattoriale di 5 = 5 * 4 * 3 * 2 * 1 = 120
     }
 }
+
+
+//    Creare una classe EserciziWhile.java che contenga un main in cui:
+//        1. Dato un intero n letto da input, si stampino a video tutti gli interi da 0 a n e poi da n a 0. Si
+//        usino sia il while che il for
+//        2. Successivamente si stampino a video tutti gli interi dispari da 0 a n
+//        3. Infine si stampi a video il fattoriale di n. Per trovare il valore si usi prima il while e poi il
+//        for. Si ricorda che il fattoriale di n è pari a: 1 * 2 * ... * (n-1) * n.
