ab3b869002386d62ae0eaca24bf2c9f563ec85bc
luca <lucabk04@gmail.com>
2022-11-21 16:51:39 +0100

    Lab 21-Nov Esercizi 1, 2, 3*

---
+++ src/Lezione8/TestInitArray.java
@@ -0,0 +1,17 @@
+package Lezione8;
+
+public class TestInitArray {
+    public static void main(String[] args) {
+        int[] array = MetodiSuArray.initArrayInt();
+        MetodiSuArray.stampaArrayInt(array);
+        int[] array2 = MetodiSuArray.initArrayInt();
+
+        MetodiSuArray.copiaElementi(array2, array);
+        MetodiSuArray.stampaArrayInt(array);
+
+        int[] a2 = MetodiSuArray.clonaArray(array);
+        MetodiSuArray.stampaArrayInt(a2);
+
+
+    }
+}
